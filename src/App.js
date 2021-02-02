464c49fcefa1d6a24e60c4f84b4afdca63120089
Ayman Omer <aymangebrilomer@gmail.com>
2021-02-01 22:49:16 -0500

    home work

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Giphy from "./componets/Giphy";
+
+function App() {
+  return (
+    <div className="App">
+      <Giphy />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/service/FatchData.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+const FatchData = async () => {
+  const apikey = "8F7a6l7hmgaVt41KDtsE5vox2ctd1Sxu";
+  const url = `https://api.giphy.com/v1/gifs/random?api_key=${apikey}`;
+  const response = await axios.get(url).then((res) => {
+    return res;
+  });
+  return response;
+};
+
+export default FatchData;
